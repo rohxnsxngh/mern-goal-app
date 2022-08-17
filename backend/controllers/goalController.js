6f5624b8ff1641e051b38900332e9e27d849685c
rohxnsxngh <rohan.singh.do@gmail.com>
2022-08-17 16:51:11 -0500

    connected to db, created schema, testing endpoints

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,10 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../model/goalModel");
+
 // Get
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 
 //Post
@@ -11,17 +14,41 @@ const createGoals = asyncHandler(async (req, res) => {
     throw new Error("Please add a text field");
   }
 
-  res.status(200).json({ message: "Create Goals" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.status(200).json(goal);
 });
 
 //Put
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update Goals ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 //Delete
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete Goals ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  await goal.remove()
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ const bodyParser = require('body-parser')
 const dotenv = require("dotenv").config();
 const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+const colors = require('colors')
 const app = express();
 
+connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(errorHandler)
